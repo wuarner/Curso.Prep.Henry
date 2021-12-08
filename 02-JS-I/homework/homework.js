193a45dc060b7c5f0a18af6d249ddf1fbbaab22c
carlos cubides <wuarnerz10@gmail.com>
2021-12-08 13:22:33 -0500

    Se realizo la comparación de x . y

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -63,6 +63,11 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+if(x === y){
+  return(true);
+}else{
+  return(false);
+}
   
 }
 
