92fdae208488ace1c21be505c211aee2a5543407
carlos cubides <wuarnerz10@gmail.com>
2021-12-08 10:57:13 -0500

    Se creo una cadena de texto

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = "Hola, padre un te amo hasta el cielo";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = null;
