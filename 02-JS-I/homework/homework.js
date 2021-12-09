8df5d818deff9dd1e56760d4d12c70e3c715db77
carlos cubides <wuarnerz10@gmail.com>
2021-12-08 20:24:23 -0500

    Comparación si  num era  impar

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -134,6 +134,11 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
+  if(num % 2 === 1){
+    return(true);
+  }else{
+    return(false);
+  }
   
 }
 
