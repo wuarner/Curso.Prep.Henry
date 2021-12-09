0bdf3f0e73221601aa0a8878effd7a3059808865
carlos cubides <wuarnerz10@gmail.com>
2021-12-09 14:30:48 -0500

    Redondeando num al entero mas proximo y devolviendo

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -173,6 +173,9 @@ function elevar(num, exponent) {
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
   // Tu código:
+  var redon = 5;
+  var redondea = Math.round(num % redon);
+  return(redondea);
   
 }
 
